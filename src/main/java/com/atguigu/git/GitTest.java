9feeea11137b8cfb48d88648ea22c1b10305b5cb
wangzhaoning1 <86098741+wangzhaoning1@users.noreply.github.com>
2021-07-24 15:41:46 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master 5! ");
         System.out.println("hot-fix 5! ");
         System.out.println("push test! ");
+        System.out.println("pull test! ");
     }
 }
