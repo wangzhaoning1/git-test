6e5e261e2c43a8ec5124b867443b20f3872d11c9
jerry_pro <a@b.c>
2021-07-24 15:29:26 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4! ");
         System.out.println("master 5! ");
         System.out.println("hot-fix 5! ");
+        System.out.println("push test! ");
     }
 }
