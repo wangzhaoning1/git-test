aca05fb78edf92881c6f4c8b7b27f18a789e1033
jerry_pro <a@b.c>
2021-07-24 15:04:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wang
+ * @date 2021/7/24 - 14:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git! ");
+    }
+}
